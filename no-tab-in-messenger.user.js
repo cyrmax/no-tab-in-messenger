45fbc1df2d51875b23e262a5a31c6d491c220540
Kirill Belousov <belousov.k.m@yandex.ru>
2023-06-17 15:55:34 +0300

    Fixed a typo which prevented script from normal work

--- no-tab-in-messenger.user.js
+++ no-tab-in-messenger.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         No tab in messenger
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Removes tab spam in facebook messenger conversations.
 // @author       Cyrmax
 // @updateURL https://cyrmax.github.io/no-tab-in-messenger
 // @downloadURL https://cyrmax.github.io/no-tab-in-messenger
 // @match        https://messenger.com/*
+// @match        https://www.messenger.com/*
 // @run-at document-start
 // @grant unsafeWindow
 // ==/UserScript==
@@ -14,11 +15,11 @@
 
 // Function that sets timer on unsafe window.
 // The timer runs every 2 seconds, searches for element with role equal to tab and tabindex equal to -1 and removes tab role on this element.
-function() {
+(function () {
     unsafeWindow.setInterval(function () {
         let element = document.querySelector('[role="tab"][tabindex="-1"]');
         if (element != null) {
             element.removeAttribute("role");
         }
     }, 2000)
-}
+})();
