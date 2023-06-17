aabe9eefcd9e965d57e838c84ab337e2e297b36b
Kirill Belousov <belousov.k.m@yandex.ru>
2023-06-17 15:28:52 +0300

    Initial commit with script code

---
+++ index.md
@@ -0,0 +1,16 @@
+## No Tab in Facebook Messenger
+
+This TamperMonkey script removes annoying "tab" on every line in every conversation in Facebook Messenger.
+
+To use this script you need a TamperMonkey browser extension.
+[Download TamperMonkey on official website](https://tampermonkey.net).
+
+### Installation
+
+After installing TamperMonkey and granting all necessary permissions just click the link bellow and agree to install user script.
+[Install script](no-tab-in-messenger.user.js).
+
+### Useful links
+
+* [Author's project list](https://cyrmax.github.io)
+* [Author's web-site](https://cyrmax.ru)
---
+++ no-tab-in-messenger.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         No tab in messenger
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Removes tab spam in facebook messenger conversations.
+// @author       Cyrmax
+// @updateURL https://cyrmax.github.io/no-tab-in-messenger
+// @downloadURL https://cyrmax.github.io/no-tab-in-messenger
+// @match        https://messenger.com/t/*
+// @run-at document-start
+// @grant unsafeWindow
+// ==/UserScript==
+
+
+// Function that sets timer on unsafe window.
+// The timer runs every 2 seconds, searches for element with role equal to tab and tabindex equal to -1 and removes tab role on this element.
+function() {
+    unsafeWindow.setInterval(function () {
+        let element = document.querySelector('[role="tab"][tabindex="-1"]');
+        if (element != null) {
+            element.removeAttribute("role");
+        }
+    }, 2000)
+}
